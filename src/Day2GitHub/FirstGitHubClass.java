b97e52fabfecf9fbffd096de6e0c6b1633488bfa
Bella <Sahnazaroveduard@gmail.com>
2023-01-31 19:24:55 -0500

    My First class creat

---
+++ src/Day2GitHub/FirstGitHubClass.java
@@ -0,0 +1,9 @@
+package Day2GitHub;
+
+public class FirstGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Batch7");
+        System.out.println("-".repeat(75));
+
+    }
+}
